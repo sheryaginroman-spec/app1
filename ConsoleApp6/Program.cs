8055cfb3356d2ec721bc6165ae16bc95b69779b7
Шерягин Роман Юрьевич <r.sheryagin@univ.priv>
2026-09-19 09:35:13 +0300

    init

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,19 @@
+﻿using ConsoleApp6.Generation;
+using ConsoleApp6.Generation.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Patterns
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Singleton.Run();
+            Factory.Run();
+        }
+    }
+}
